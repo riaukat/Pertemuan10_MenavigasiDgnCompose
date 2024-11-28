3cb7fde6e65f08fcb96e259729572d6756049869
riaukat <139480111+riaukat@users.noreply.github.com>
2024-11-28 19:19:28 +0700

    Update CommonUi.kt

--- app/src/main/java/com/valeriaukat/cupcake/ui/components/CommonUi.kt
+++ app/src/main/java/com/valeriaukat/cupcake/ui/components/CommonUi.kt
@@ -1,20 +1,22 @@
-package com.valeriaukat.cupcake.ui.components
+package com.valeriaukat.cupcake.ui.components 
 
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.valeriaukat.cupcake.R
-
+import androidx.compose.material3.MaterialTheme 
+import androidx.compose.material3.Text 
+import androidx.compose.runtime.Composable 
+import androidx.compose.ui.Modifier 
+import androidx.compose.ui.res.stringResource 
+import com.valeriaukat.cupcake.R 
 /**
  * Composable that displays formatted [price] that will be formatted and displayed on screen
  */
 @Composable
-fun FormattedPriceLabel(subtotal: String, modifier: Modifier = Modifier) {
-    Text(
-        text = stringResource(R.string.subtotal_price, subtotal),
-        modifier = modifier,
-        style = MaterialTheme.typography.headlineSmall
+fun FormattedPriceLabel( // Fungsi komposisi untuk menampilkan label harga yang diformat
+    subtotal: String, // Parameter untuk subtotal harga
+    modifier: Modifier = Modifier // Parameter untuk Modifier, default ke Modifier kosong
+) {
+    Text( // Menampilkan teks
+        text = stringResource(R.string.subtotal_price, subtotal), // Mengambil dan memformat string untuk subtotal
+        modifier = modifier, // Menerapkan modifier untuk teks
+        style = MaterialTheme.typography.headlineSmall // Mengatur gaya teks menggunakan tema
     )
 }
