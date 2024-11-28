383b80e5e45ae1b4e801e3f75092cb1e35b1f10d
riaukat <139480111+riaukat@users.noreply.github.com>
2024-11-28 18:36:16 +0700

    Update MainActivity.kt

--- app/src/main/java/com/valeriaukat/cupcake/MainActivity.kt
+++ app/src/main/java/com/valeriaukat/cupcake/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.valeriaukat.cupcake.ui.theme.CupcakeTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
-        super.onCreate(savedInstanceState)
-        setContent {
-            CupcakeTheme {
-                CupcakeApp()
+class MainActivity : ComponentActivity() { // Mendeklarasikan kelas MainActivity yang mewarisi ComponentActivity
+    override fun onCreate(savedInstanceState: Bundle?) { // Mengoverride metode onCreate, dipanggil saat aktivitas dibuat
+        enableEdgeToEdge() // Mengaktifkan mode tampilan edge-to-edge untuk aktivitas ini
+        super.onCreate(savedInstanceState) // Memanggil onCreate dari kelas induk untuk inisialisasi
+        setContent { // Mengatur konten tampilan menggunakan fungsi lambda
+            CupcakeTheme { // Menerapkan tema Cupcake pada konten
+                CupcakeApp() // Memanggil fungsi CupcakeApp untuk menampilkan antarmuka aplikasi
             }
         }
     }
